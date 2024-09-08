fa89ad3451d028faec5c70d002588ce52841ea10
Nosipho2021 <lorrainemdk@gmail.com>
2024-09-08 20:41:06 +0000

    update

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel for internationalization (i18n).
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration class for Flask app and Babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Select and return the best matching language.
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handle the root URL '/' and render the 4-index.html template.
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
+
