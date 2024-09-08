7c61c676d3c7e458130a8c3f602d665fbf876287
Nosipho2021 <lorrainemdk@gmail.com>
2024-09-08 20:46:07 +0000

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-""" Parametrize templates with translations.
-"""
+""" get local from request """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel
 
 
 class Config:
-    """ Configuration class for Babel and supported languages. """
+    """ Config class for app """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,18 +19,15 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """ Get locale from request and match with supported languages. """
+    """ Get locale from request """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
 def welcome() -> str:
-    """ Render the home page with translated content. """
-    return render_template('3-index.html', 
-                           home_title=_('home_title'), 
-                           home_header=_('home_header'))
+    """ / page """
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
